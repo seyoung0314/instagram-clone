37187ed311b80002f86bf51f7225fdb49ebaf806
samchi <seyoung0314@gmail.com>
2024-12-31 12:25:46 +0900

    [F/refactor] refactoring modal open event

--- src/main/resources/static/js/components/create-feed-modal.js
+++ src/main/resources/static/js/components/create-feed-modal.js
@@ -1,8 +1,8 @@
 //피드 생성 모달을 전역관리
 let $modal = null;
 
-//모달 관련 js 함수 - 외부에 노출
-function initCreateFeedModal() {
+// 피드 생성 모달 간련 이벤트 함수
+function setUpModalEvents(){
   $modal = document.getElementById("createPostModal");
 
   // 모달 열기 함수
@@ -17,6 +17,16 @@ function initCreateFeedModal() {
     .querySelector(".fa-square-plus")
     .closest(".menu-item")
     .addEventListener("click", openModal);
+}
+
+// 이벤트 바인딩 관련 함수
+function bindEvents(){
+  setUpModalEvents();
+}
+
+//모달 관련 js 함수 - 외부에 노출
+function initCreateFeedModal() {
+  bindEvents();
 }
 
 export default initCreateFeedModal();
