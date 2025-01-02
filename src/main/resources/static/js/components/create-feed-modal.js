40f49c2ff8328802931816d389869776c44ecbf6
samchi <seyoung0314@gmail.com>
2025-01-02 11:37:26 +0900

    [FE/feat] image upload binding drag and drop event

--- src/main/resources/static/js/components/create-feed-modal.js
+++ src/main/resources/static/js/components/create-feed-modal.js
@@ -1,9 +1,7 @@
-
 import CarouselManager from "../ui/CarouselManager.js";
 
 //step 모듈내에서 전역관리
 let currentStep = 1;
-
 
 let step2Carousel = null;
 let step3Carousel = null;
@@ -20,6 +18,7 @@ let element = {
   $backStepBtn: $modal.querySelector(".back-button"),
   $nextStepBtn: $modal.querySelector(".next-button"),
   $modalTitle: $modal.querySelector(".modal-title"),
+  $uploadArea: $modal.querySelector(".upload-area"),
 };
 
 // 모달 바디 스텝을 이동하는 함수
@@ -28,7 +27,7 @@ function goToStep(step) {
 
   currentStep = step;
 
-  const { $backStepBtn, $nextStepBtn, $modalTitle } = element;
+  const { $backStepBtn, $nextStepBtn, $modalTitle, $fileInput } = element;
 
   // active 클래스 포함 시 이동
   [...$modal.querySelectorAll(".step")].forEach(($step, index) => {
@@ -44,25 +43,25 @@ function goToStep(step) {
 
     if (step === 1) {
       // 다음번 change이벤트 발동을 위해 초기화
-      $fileInput.value = ''; 
-      $nextStepBtn.style.display = 'none';
-      $backStepBtn.style.visibility = 'hidden';
-      $modalTitle.textContent = '새 게시물 만들기';
+      $fileInput.value = "";
+      $nextStepBtn.style.display = "none";
+      $backStepBtn.style.visibility = "hidden";
+      $modalTitle.textContent = "새 게시물 만들기";
     } else if (step === 2) {
-      $nextStepBtn.style.display = 'block';
-      $backStepBtn.style.visibility = 'visible';
-      $modalTitle.textContent = '편집';
-      $nextStepBtn.textContent = '다음';
+      $nextStepBtn.style.display = "block";
+      $backStepBtn.style.visibility = "visible";
+      $modalTitle.textContent = "편집";
+      $nextStepBtn.textContent = "다음";
     } else if (step === 3) {
-      $nextStepBtn.textContent = '공유하기';
-      $modalTitle.textContent = '새 게시물 만들기';
+      $nextStepBtn.textContent = "공유하기";
+      $modalTitle.textContent = "새 게시물 만들기";
     }
   });
 }
 
 // 파일 업로드 관련 이벤트 함수
 function setUpFileUploadEvents() {
-  const { $uploadBtn, $fileInput } = element;
+  const { $uploadBtn, $fileInput, $uploadArea } = element;
 
   // 파일을 검사하고 다음 단계로 이동하는 함수
   const handleFiles = (files) => {
@@ -72,7 +71,7 @@ function setUpFileUploadEvents() {
       return;
     }
 
-    //파일이 10MB 이하의 이미지 파일인지 검증증
+    //파일이 10MB 이하의 이미지 파일인지 검증
     // console.log(files);
     const validFiles = files
       .filter((file) => {
@@ -90,18 +89,22 @@ function setUpFileUploadEvents() {
         return true;
       });
 
-      //이미지 슬라이드 생성 
-      //기존 캐러셀이 없을 시에만 생성 (중복방지)
-      if(!step2Carousel){   
-        step2Carousel = new CarouselManager($modal.querySelector('.preview-container'));
-      }
-      step2Carousel.init(validFiles);
+    //이미지 슬라이드 생성
+    //기존 캐러셀이 없을 시에만 생성 (중복방지)
+    if (!step2Carousel) {
+      step2Carousel = new CarouselManager(
+        $modal.querySelector(".preview-container")
+      );
+    }
+    step2Carousel.init(validFiles);
 
-      //이미지 슬라이드 생성
-      if(!step3Carousel){
-        step3Carousel = new CarouselManager($modal.querySelector('.write-container'));
-      }
-      step3Carousel.init(validFiles);
+    //이미지 슬라이드 생성
+    if (!step3Carousel) {
+      step3Carousel = new CarouselManager(
+        $modal.querySelector(".write-container")
+      );
+    }
+    step3Carousel.init(validFiles);
 
     // 모달 step2(이미지 슬라이드)로 이동
     goToStep(2);
@@ -118,6 +121,32 @@ function setUpFileUploadEvents() {
     if (files.length > 0) {
       handleFiles(files);
     }
+  });
+
+  // 파일 드래그앤드롭 이벤트
+  // 드래그 영역에 진입 시
+  $uploadArea.addEventListener("dragover", (e) => {
+    e.preventDefault();
+    $uploadArea.classList.add("dragover");
+  });
+
+  // 드래그 영역을 벗어났을 시
+  $uploadArea.addEventListener("dragleave", (e) => {
+    e.preventDefault();
+    $uploadArea.classList.remove("dragover");
+  });
+  // 드래그 영역에 놓았을 시
+
+  $uploadArea.addEventListener("drop", (e) => {
+    e.preventDefault(); // 드롭했을 시 이미지 새탭이 열리거나 파일다운로드 방지
+
+    //콘솔로그에서 안찍힘
+    console.log(e);
+
+    //파일정보 얻어오기
+    const files = [...e.dataTransfer.files];
+    //파일검증
+    if (files.length > 0) handleFiles(files);
   });
 }
 
