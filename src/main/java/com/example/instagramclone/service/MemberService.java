ce32df4756ab18ccbeb5c640e00fc1002651af5d
samchi <seyoung0314@gmail.com>
2025-01-08 12:34:47 +0900

    [B/feat] encoding password

--- src/main/java/com/example/instagramclone/service/MemberService.java
+++ src/main/java/com/example/instagramclone/service/MemberService.java
@@ -1,9 +1,11 @@
 package com.example.instagramclone.service;
 
 import com.example.instagramclone.domain.member.dto.SignUpRequest;
+import com.example.instagramclone.domain.member.entity.Member;
 import com.example.instagramclone.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,12 +15,25 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class MemberService {
 
+    //패스워드 암호화
+    private final PasswordEncoder passwordEncoder;
     private final MemberRepository memberRepository;
 
     //회원가입 중간처리
     public void signup(SignUpRequest signUpRequest){
+
+        // 숩수 비밀번호를 꺼내서 암호화
+        String rawPassword = signUpRequest.getPassword();
+        String encodedPassword = passwordEncoder.encode(rawPassword);
+
         //회원정보를 데이터베이스에 저장
-        memberRepository.insert(signUpRequest.toEntity());
+        Member newMember = signUpRequest.toEntity();
+
+        //암호화된 pw로 교체
+        newMember.setPassword(encodedPassword);
+
+        // Db로 전송
+        memberRepository.insert(newMember);
 
     }
 }
