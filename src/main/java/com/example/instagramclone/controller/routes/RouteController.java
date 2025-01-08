549cdb7b05fca63c76115321790d3f6521157343
samchi <seyoung0314@gmail.com>
2025-01-08 11:01:07 +0900

    [F/feat] add auth pages and styles

--- src/main/java/com/example/instagramclone/controller/routes/RouteController.java
+++ src/main/java/com/example/instagramclone/controller/routes/RouteController.java
@@ -9,7 +9,15 @@ public class RouteController {
 
     @GetMapping("/")
     public String index() {
-        return "index";
+        // 나중에 로그인 후 복귀할떄 주석해제
+//        return "index";
+        return "auth/login";
     }
 
+    //회원가입 페이지 열기
+    // 회원가입 페이지 열기
+    @GetMapping("/signup")
+    public String signUp() {
+        return "auth/signup";
+    }
 }
