82c301f86e251a41a69f55af493240b5f54caf72
samchi <seyoung0314@gmail.com>
2025-01-09 12:09:44 +0900

    [B/feat] make checking duplicate value API

--- src/main/java/com/example/instagramclone/controller/rest/AuthController.java
+++ src/main/java/com/example/instagramclone/controller/rest/AuthController.java
@@ -1,15 +1,13 @@
 package com.example.instagramclone.controller.rest;
 
 import com.example.instagramclone.domain.member.dto.SignUpRequest;
+import com.example.instagramclone.domain.member.dto.response.DuplicateCheckResponse;
 import com.example.instagramclone.service.MemberService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
@@ -32,5 +30,15 @@ public class AuthController {
                         "message","회원가입이 완료되었습니다.",
                         "username",signUpRequest.getUsername()
                 ));
+    }
+    // 중복확인을 검사하는 api
+    @GetMapping("/check-duplicate")
+    public ResponseEntity<?> checkDuplicate(
+            @RequestParam String type,
+            @RequestParam String value
+    ){
+        log.info("check duplicate type:{},value:{}",type,value);
+        DuplicateCheckResponse responsedto = memberService.checkDuplicate(type, value);
+        return ResponseEntity.ok().body(responsedto);
     }
 }
--- src/main/java/com/example/instagramclone/service/MemberService.java
+++ src/main/java/com/example/instagramclone/service/MemberService.java
@@ -1,13 +1,18 @@
 package com.example.instagramclone.service;
 
 import com.example.instagramclone.domain.member.dto.SignUpRequest;
+import com.example.instagramclone.domain.member.dto.response.DuplicateCheckResponse;
 import com.example.instagramclone.domain.member.entity.Member;
+import com.example.instagramclone.exception.ErrorCode;
+import com.example.instagramclone.exception.MemberException;
 import com.example.instagramclone.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @Slf4j
@@ -20,7 +25,7 @@ public class MemberService {
     private final MemberRepository memberRepository;
 
     //회원가입 중간처리
-    public void signup(SignUpRequest signUpRequest){
+    public void signup(SignUpRequest signUpRequest) {
 
         // 숩수 비밀번호를 꺼내서 암호화
         String rawPassword = signUpRequest.getPassword();
@@ -34,6 +39,32 @@ public class MemberService {
 
         // Db로 전송
         memberRepository.insert(newMember);
+    }
 
+    /**
+     * 중복 검사 통합 처리 (이메일, 전화번호, 유저네임)
+     *
+     * @param type  - 검사랄 값의 타입(email,phone,usename)
+     * @param value - 중복 검사할 값
+     */
+    public DuplicateCheckResponse checkDuplicate(String type, String value) {
+        switch (type) {
+            case "email":
+                // 중복된 경우를 클라이언트에게 알려야함
+                return memberRepository.findByEmail(value)
+                        .map(member -> DuplicateCheckResponse.unavailable("이미 사용중인 이메일입니다.")) // null이 아닌 경우
+                        .orElse(DuplicateCheckResponse.available());// null인 경우
+            case "phone":
+                return memberRepository.findByPhone(value.replaceAll("[^0-9]", ""))
+                        .map(member -> DuplicateCheckResponse.unavailable("이미 사용중인 전화번호입니다.")) // null이 아닌 경우
+                        .orElse(DuplicateCheckResponse.available());// null인 경우;
+            case "username":
+                return memberRepository.findByUsername(value)
+                        .map(member -> DuplicateCheckResponse.unavailable("이미 사용중인 이름입니다.")) // null이 아닌 경우
+                        .orElse(DuplicateCheckResponse.available());// null인 경우;
+            default:
+                throw new MemberException(ErrorCode.INVALID_SIGNUP_DATA);
+
+        }
     }
 }
