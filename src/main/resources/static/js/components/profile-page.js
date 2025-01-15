dc10bfb7737e3be3d6744bec9171ebd1fc04b9d2
samchi <seyoung0314@gmail.com>
2025-01-15 12:02:57 +0900

    [F/feat] profile action button and bookmark navi-bar conditional rendering

--- src/main/resources/static/js/components/profile-page.js
+++ src/main/resources/static/js/components/profile-page.js
@@ -1,16 +1,16 @@
-
 import { fetchWithAuth } from "../util/api.js";
 import initCreateFeedModal from "./create-feed-modal.js";
 import initMoreMenu from "./more-menu.js";
 import initSideBar from "./side-bar.js";
+import { getCurrentUser } from "../util/auth.js";
 
-function getPageUsername(){
+function getPageUsername() {
   const url = window.location.pathname;
   return url.substring(1);
 }
 
-// 프로필 페이지 상단부 렌더링 
-async function initProfileHeader(){
+// 프로필 페이지 상단부 렌더링
+async function initProfileHeader() {
   //해당 페이지 사용자이름 url에서 추출
   const username = getPageUsername();
 
@@ -20,35 +20,73 @@ async function initProfileHeader(){
 
   //렌더링
   renderProfileHeader(profileHeader);
-  
+}
+
+// 현재 페이지가 들어온 사람이 본인인지 확인
+async function isUserMatched() {
+  const pageUsername = getPageUsername();
+  const loggedInUser = await getCurrentUser();
+  return pageUsername === loggedInUser.username;
 }
 
 // 프로필 헤더 렌더링하기
-function renderProfileHeader({ feedCount, name, username, profileImageUrl }) {
+async function renderProfileHeader({
+  feedCount,
+  name,
+  username,
+  profileImageUrl,
+}) {
   // 프로필 이미지 업데이트
-  document.querySelector('.profile-image-container .profile-image img').src =
-    profileImageUrl ?? '/images/default-profile.svg';
+  document.querySelector(".profile-image-container .profile-image img").src =
+    profileImageUrl ?? "/images/default-profile.svg";
 
   // 사용자명 업데이트
-  document.querySelector('.profile-actions .username').textContent = username;
+  document.querySelector(".profile-actions .username").textContent = username;
 
   // 실명 업데이트
-  document.querySelector('.profile-bio .full-name').textContent = name;
+  document.querySelector(".profile-bio .full-name").textContent = name;
 
   // 게시물 수 업데이트
   document.querySelector(
-    '.profile-stats li:first-child .stats-number'
+    ".profile-stats li:first-child .stats-number"
   ).textContent = feedCount;
+
+  // 본인 페이지인지 확인
+  const match = await isUserMatched();
+
+  // 버튼 영역 렌더링
+  const $actionButtonsContainer = document.querySelector(".action-buttons");
+  $actionButtonsContainer.innerHTML = "";
+
+  const $naviBookMark = document.querySelector(".profile-nav a:nth-child(3)");
+
+  if (match) {
+    // 본인 프로필인 경우
+    $actionButtonsContainer.innerHTML = `
+              <button class="profile-edit-button">프로필 편집</button>
+              <button class="settings-button">
+                  <i class="fa-solid fa-gear"></i>
+              </button>
+          `;
+
+    $naviBookMark.style.display = "block";
+  } else {
+    // 타인 프로필인 경우
+    $actionButtonsContainer.innerHTML = `
+              <button class="follow-button">팔로우</button>
+              <button class="message-button">메시지 보내기</button>
+          `;
+
+    $naviBookMark.style.display = "none";
+  }
 }
 
-document.addEventListener('DOMContentLoaded',()=>{
-
+document.addEventListener("DOMContentLoaded", () => {
   // 인덱스 페이지와 공통처리
-  initCreateFeedModal();  //피드 생성관련 js
+  initCreateFeedModal(); //피드 생성관련 js
   initMoreMenu(); // 더보기 버튼 클릭 관련 js
-  initSideBar();  // 사이드바 관련
-  
+  initSideBar(); // 사이드바 관련
+
   //프로필 페이지 개별 처리
   initProfileHeader(); // 프로필 페이지 헤더 관련
-  
 });
