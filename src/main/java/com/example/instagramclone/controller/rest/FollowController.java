7789da6291393d3e7de58c787b0ae83bcacd4afa
samchi <seyoung0314@gmail.com>
2025-01-31 09:23:12 +0900

    [B/feat] make get follow list API

--- src/main/java/com/example/instagramclone/controller/rest/FollowController.java
+++ src/main/java/com/example/instagramclone/controller/rest/FollowController.java
@@ -1,17 +1,19 @@
 package com.example.instagramclone.controller.rest;
 
+import com.example.instagramclone.domain.follow.dto.response.FollowResponse;
 import com.example.instagramclone.service.FollowService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+
+import static com.example.instagramclone.domain.follow.dto.response.FollowStatus.FOLLOWER;
+import static com.example.instagramclone.domain.follow.dto.response.FollowStatus.FOLLOWING;
 
 @RestController
 @RequiredArgsConstructor
@@ -29,5 +31,27 @@ public class FollowController {
 
         Map<String, Object> responseMap = followService.toggleFollow(followingName, followerName);
         return ResponseEntity.ok().body(responseMap);
+    }
+
+
+    // 팔로워 리스트 조회 API
+    @GetMapping("/{targetUsername}/followers")
+    public ResponseEntity<?> getFollowers(
+            @PathVariable String targetUsername
+            , @AuthenticationPrincipal String loginUsername
+    ) {
+        List<FollowResponse> followers
+                = followService.getFollows(targetUsername, loginUsername, FOLLOWER);
+        return ResponseEntity.ok().body(followers);
+    }
+    // 팔로잉 리스트 조회 API
+    @GetMapping("/{targetUsername}/followings")
+    public ResponseEntity<?> getFollowings(
+            @PathVariable String targetUsername
+            , @AuthenticationPrincipal String loginUsername
+    ) {
+        List<FollowResponse> followings
+                = followService.getFollows(targetUsername, loginUsername, FOLLOWING);
+        return ResponseEntity.ok().body(followings);
     }
 }
